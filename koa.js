434a621e01dede24fe653dbf7ed99e449312dab4
avwo <avwu@qq.com>
2017-02-27 00:59:11 +0800

    Init

--- koa.js
+++ koa.js
@@ -3,7 +3,7 @@ var proxy = require('./lib');
 module.exports = function (options) {
   var intercept = proxy.start(options);
   return function* (next) {
-    if (!intercept(this.req, this.res)) {
+    if (!(yield intercept(this.req, this.res))) {
       yield next;
     }
   };
