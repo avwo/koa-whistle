9cb50f6d6b0cbb2376837f6c677af8e664de56d4
avwo <avwu@qq.com>
2017-02-27 00:35:15 +0800

    Init

--- koa.js
+++ koa.js
@@ -3,10 +3,9 @@ var proxy = require('./lib');
 module.exports = function (options) {
   var intercept = proxy.start(options);
   return function* (next) {
-    if (intercept(this.req, this.res)) {
-      return;
+    if (!intercept(this.req, this.res)) {
+      yield next;
     }
-    yield next;
   };
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,14 @@
 var Koa = require('koa');
 var app = new Koa();
 var useProxy = require('../koa');
+var serverPort = 7001;
 
+app.use(useProxy({
+  name: 'test',
+  serverPort: serverPort
+}));
+app.use(function* (next) {
+  this.body = 'Hello world!';
+});
 
+app.listen(serverPort);
