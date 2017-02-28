29e943bde6be475fe94591939068b1bcfd8d6212
avwo <avwu@qq.com>
2017-02-28 23:35:23 +0800

    test: Init

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,7 @@
 require('./koa.test');
 require('./express.test');
 require('./units/connect.test');
+require('./units/host.test');
 require('./units/proxy.test');
+require('./units/socks.test');
+require('./units/request.test');
