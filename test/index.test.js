b831ad88b989107699870f03ca8733a08531a8a3
avwo <avwu@qq.com>
2017-05-19 20:09:19 +0800

    test: Init

--- test/index.test.js
+++ test/index.test.js
@@ -8,13 +8,19 @@ const expressServer = require('./express.test');
 
 const baseDir = path.join(__dirname, '../');
 
-proxy.getRandomPort().then((port) => {
-  Promise.all([
-    proxy.startWhistle({ port, baseDir, name: 'test-koa-whistle' }),
-    util.startHTTPServer(),
-    util.startHTTPsServer(),
-    util.startSocketServer(),
-  ]).then(() => {
+before((done) => {
+  proxy.getRandomPort().then((port) => {
+    Promise.all([
+      proxy.startWhistle({ port, baseDir, name: 'test-koa-whistle' }),
+      util.startHTTPServer(),
+      util.startHTTPsServer(),
+      util.startSocketServer(),
+    ]).then(() => done);
+  });
+});
+
+describe('#connect', () => {
+  it('proxy.connect', (done) => {
     proxy.connect({
       host: 'www.test.com',
       rules: `www.test.com 127.0.0.1:${util.socketServerPort}`,
@@ -23,26 +29,40 @@ proxy.getRandomPort().then((port) => {
         assert(`${data}` === 'socket', 'Expected to return string socket.');
       });
     });
+  });
+});
+
+describe('#request', () => {
+  it('https proxy.request(options, cb)', (done) => {
     proxy.request({
       url: 'https://www.test.com/index.html',
       rules: [`www.test.com 127.0.0.1:${util.httpsServerPort}`],
     }, (err, res, body) => {
       assert(body === 'HTTPs', 'Expected to return string HTTPs.');
+      done();
     });
+  });
+  it('https proxy.request(options)', (done) => {
     proxy.request({
       url: 'https://www.test.com/index.html',
       rules: `www.test.com 127.0.0.1:${util.httpsServerPort}`,
     }).then((res) => {
       res.on('data', (data) => {
         assert(`${data}` === 'HTTPs', 'Expected to return string HTTPs.');
+        done();
       });
     });
+  });
+  it('http proxy.request(options, cb)', (done) => {
     proxy.request({
       url: 'http://www.test.com/index.html',
       rules: `www.test.com 127.0.0.1:${util.httpServerPort}`,
     }, (err, res, body) => {
       assert(body === 'HTTP', 'Expected to return string HTTP.');
+      done();
     });
+  });
+  it('http proxy.request(options,)', (done) => {
     proxy.request({
       url: 'http://www.test.com/index.html',
       method: 'post',
@@ -50,83 +70,95 @@ proxy.getRandomPort().then((port) => {
     }).then((res) => {
       res.on('data', (data) => {
         assert(`${data}` === 'HTTP', 'Expected to return string HTTP.');
+        done();
       });
     });
+  });
+});
 
-    Promise.all([koaServer(), expressServer()]).then(([koaPort, expressPort]) => {
-      request(`https://127.0.0.1:${koaPort}`, (err, res, body) => {
-        assert(body === 'koa', 'Expected to return string koa.');
-      });
-      request({
-        uri: `http://127.0.0.1:${expressPort}`,
-        method: 'post',
-      }, (err, res, body) => {
-        assert(body === 'express', 'Expected to return string express.');
-      });
-      request(`https://127.0.0.1:${koaPort}${util.PATHNAME}`, (err, res, body) => {
-        assert(/<script/.test(body), 'Expected to return whistle ui.');
-      });
-      request(`http://127.0.0.1:${expressPort}${util.PATHNAME}`, (err, res, body) => {
-        assert(/<script/.test(body), 'Expected to return whistle ui page.');
-      });
+describe('#koa middleware', () => {
+  let koaPort;
+  before((done) => {
+    koaServer().then((port) => {
+      koaPort = port;
+      done();
+    });
+  });
+  it('request', (done) => {
+    request(`https://127.0.0.1:${koaPort}`, (err, res, body) => {
+      assert(body === 'koa', 'Expected to return string koa.');
+      done();
+    });
+  });
+  it('whistle', (done) => {
+    request(`https://127.0.0.1:${koaPort}${util.PATHNAME}`, (err, res, body) => {
+      assert(/<script/.test(body), 'Expected to return whistle ui.');
+      done();
+    });
+  });
+  it('by domain', (done) => {
+    request(util.formatOptions({
+      url: 'https://www.test.com/index.html',
+      host: '127.0.0.1',
+      port: koaPort,
+    }), (err, res, body) => {
+      assert(body === 'index.html', 'Expected to return string koa.');
+      done();
+    });
+  });
+  it('1. not match', (done) => {
+    request(util.formatOptions({
+      url: 'https://www.test.com/index2.html',
+      host: '127.0.0.1',
+      port: koaPort,
+    }), (err, res, body) => {
+      assert(body === 'koa', 'Expected to return string koa.');
+      done();
+    });
+  });
+  it('2. not match', (done) => {
+    request(util.formatOptions({
+      url: 'https://www.test.com/cgi-bin/get2',
+      host: '127.0.0.1',
+      port: koaPort,
+    }), (err, res, body) => {
+      assert(body === 'koa', 'Expected to return string koa.');
+      done();
+    });
+  });
+});
 
-      request(util.formatOptions({
-        url: 'https://www.test.com/index.html',
-        host: '127.0.0.1',
-        port: koaPort,
-      }), (err, res, body) => {
-        assert(body === 'index.html', 'Expected to return string koa.');
-      });
-      request(util.formatOptions({
-        url: 'https://www.test.com/cgi-bin/get',
-        host: '127.0.0.1',
-        port: koaPort,
-      }), (err, res, body) => {
-        assert(body === 'test.json', 'Expected to return string koa.');
-      });
-      request(util.formatOptions({
-        url: 'https://www.test.com/index2.html',
-        host: '127.0.0.1',
-        port: koaPort,
-      }), (err, res, body) => {
-        assert(body === 'koa', 'Expected to return string koa.');
-      });
-      request(util.formatOptions({
-        url: 'https://www.test.com/cgi-bin/get2',
-        host: '127.0.0.1',
-        port: koaPort,
-      }), (err, res, body) => {
-        assert(body === 'koa', 'Expected to return string koa.');
-      });
-
-      request(util.formatOptions({
-        url: 'http://www.test.com/index.html',
-        host: '127.0.0.1',
-        port: expressPort,
-      }), (err, res, body) => {
-        assert(body === 'express', 'Expected to return string express.');
-      });
-      request(util.formatOptions({
-        url: 'http://www.test.com/cgi-bin/get',
-        host: '127.0.0.1',
-        port: expressPort,
-      }), (err, res, body) => {
-        assert(body === 'express', 'Expected to return string express.');
-      });
-      request(util.formatOptions({
-        url: 'http://www.test.com/index2.html',
-        host: '127.0.0.1',
-        port: expressPort,
-      }), (err, res, body) => {
-        assert(body === 'express', 'Expected to return string express.');
-      });
-      request(util.formatOptions({
-        url: 'http://www.test.com/cgi-bin/get2',
-        host: '127.0.0.1',
-        port: expressPort,
-      }), (err, res, body) => {
-        assert(body === 'express', 'Expected to return string express.');
-      });
+describe('#express middleware', () => {
+  let expressPort;
+  before((done) => {
+    expressServer().then((port) => {
+      expressPort = port;
+      done();
+    });
+  });
+  it('request', (done) => {
+    request({
+      uri: `http://127.0.0.1:${expressPort}`,
+      method: 'post',
+    }, (err, res, body) => {
+      assert(body === 'express', 'Expected to return string express.');
+      done();
+    });
+  });
+  it('whistle', (done) => {
+    request(`http://127.0.0.1:${expressPort}${util.PATHNAME}`, (err, res, body) => {
+      assert(/<script/.test(body), 'Expected to return whistle ui page.');
+      done();
+    });
+  });
+  it('by domain', (done) => {
+    request(util.formatOptions({
+      url: 'http://www.test.com/cgi-bin/get2',
+      host: '127.0.0.1',
+      port: expressPort,
+    }), (err, res, body) => {
+      assert(body === 'express', 'Expected to return string express.');
+      done();
     });
   });
 });
